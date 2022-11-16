f297cc56f0eeb776215e1870321a71ab0ac0e2c3
r83wheeler <63355207+r83wheeler@users.noreply.github.com>
2022-11-15 20:55:22 -0500

    update

--- listBuild.py
+++ listBuild.py
@@ -1,11 +1,30 @@
 stuff = list() 
 stuff.append('book')
 stuff.append(99)
-print(stuff)
+#print(stuff)
 
 stuff.append('cookie')
-print(stuff)
+#print(stuff)
 
 
 some = [1, 9, 21, 10, 16]
 9 in some 
+
+friends = ['Joseph', 'Glenn', 'Sally']
+friends.sort()
+
+print(friends)
+print(friends[2])
+
+nums = [3, 41, 12, 9, 74, 15]
+
+print(len(nums))
+
+print(max(nums))
+
+print(min(nums))
+
+print(sum(nums))
+
+print(sum(nums)/len(nums))
+
