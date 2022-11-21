027d2710d92e8667c7161cd5ff939447052da91e
r83wheeler <63355207+r83wheeler@users.noreply.github.com>
2022-11-20 20:50:30 -0500

    words.py

--- counts.py
+++ counts.py
@@ -2,3 +2,11 @@ counts = {'chuck' : 1, 'fred' : 42, 'jan' : 100}
 for key in counts:
     print(key, counts[key])
 
+jjj = {'chuck' : 1, 'fred' : 42, 'jan' : 100}
+print(list(jjj))
+
+print(jjj.keys())
+
+print(jjj.values())
+
+print(jjj.items())
